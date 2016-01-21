e58e11c0ce1f3484955447eddeeec8d15dcfe552
alinaw89 <ali.naw89@gmail.com>
2016-01-21 14:26:42 -0500

    added basic html, css to set up. wrote some jquery

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("button").click(function() {
+    $(".quotes").html("<p>you did it!</p>")
+  });
+});
+
+//1.have a message pop up if someone clicks on the button
+//2.have a message pop up in a box if someone clicks the button
+//3.how to set up an api with a free quotes source
+//4.redesign
